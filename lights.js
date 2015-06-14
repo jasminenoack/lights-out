1152fb0c6d99491d7055df51a0bac7e991ef5fbe
Jasmine Noack <jasminenoack@gmail.com>
2015-06-13 23:22:36 -0400

    checks win in console

--- lights.js
+++ lights.js
@@ -1,6 +1,43 @@
 var setup = function () {
-    window.blocks = $("li")
-    console.log(blocks.length)
-        // .chunk(5)
-        // console.log(blocks)
+    $("li").click(handleClick)
+}
+
+function getBlock (index) {
+    return $($("li")[index])
+}
+
+var handleClick = function (event) {
+    var block = $(event.currentTarget)
+    var index = block.index()
+    var left = index % 5 == 0
+    var right = (index + 1) % 5 == 0
+    var top = index < 5
+    var bottom = index > 19
+
+    block.toggleClass("on")
+    if (!top) {
+        above = getBlock(index - 5).toggleClass("on")
+    }
+    if (!bottom) {
+        above = getBlock(index + 5).toggleClass("on")
+    }
+    if (!left) {
+        above = getBlock(index - 1).toggleClass("on")
+    }
+    if (!right) {
+        above = getBlock(index + 1).toggleClass("on")
+    }
+
+    if (isComplete()) {
+        console.log("You win!")
+    }
+}
+
+function isComplete () {
+    if (!$(".on").length) {
+        return true
+    }
+    else {
+        return false
+    }
 }
